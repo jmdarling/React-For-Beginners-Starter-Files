bad5a0b8c75603d8881758174a5ad21c68f25b18
Jonathan Darling <jmdarling@me.com>
2016-11-16 12:19:39 -0600

    Saving progress after 'Helper and Utility Functions' lesson.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,14 +1,18 @@
 'use strict'
 
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+
+import { getFunName } from './../helpers.js'
 
 class StorePicker extends Component {
   render () {
     return (
       <form className='store-selector'>
         <h2>Please Enter a Store</h2>
-        <input type='text' placeholder='Store Name' required />
-        <button type='submit'>Visit Store</button>
+        <input type='text' placeholder={getFunName()} required />
+        <button type='submit'>
+          Visit Store
+        </button>
       </form>
     )
   }
