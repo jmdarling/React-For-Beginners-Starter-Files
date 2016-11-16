a6a67ad4b1fbbe28292cdafe2273359169fd3143
Jonathan Darling <jmdarling@me.com>
2016-11-16 12:58:43 -0600

    Saving progress after 'Working with React Events' lesson.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -5,11 +5,33 @@ import React, { Component } from 'react'
 import { getFunName } from './../helpers.js'
 
 class StorePicker extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      storeNameInput: ''
+    }
+
+    this.onStoreNameChanged = this.onStoreNameChanged.bind(this)
+    this.onStorePickerSubmitted = this.onStorePickerSubmitted.bind(this)
+  }
+
+  onStoreNameChanged (newValue) {
+    this.setState({
+      storeNameInput: newValue
+    })
+  }
+
+  onStorePickerSubmitted (event) {
+    event.preventDefault()
+  }
+
   render () {
     return (
-      <form className='store-selector'>
+      <form className='store-selector' onSubmit={this.onStorePickerSubmitted}>
         <h2>Please Enter a Store</h2>
-        <input type='text' placeholder={getFunName()} required />
+        <input type='text' onChange={(event) => this.onStoreNameChanged(event.target.value)} placeholder={getFunName()} value={this.state.storeNameInput} required />
         <button type='submit'>
           Visit Store
         </button>
